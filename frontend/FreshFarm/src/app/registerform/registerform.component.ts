462cd6ba77eb780c31f1cb121d89d83efee71786
Jabrane Ben Hadj Messaoud <jabrane.benhadjmessaoud@gmail.com>
2025-01-28 18:48:40 +0100

    adding angular components

---
+++ frontend/FreshFarm/src/app/registerform/registerform.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registerform',
+  imports: [],
+  templateUrl: './registerform.component.html',
+  styleUrl: './registerform.component.css'
+})
+export class RegisterformComponent {
+
+}
